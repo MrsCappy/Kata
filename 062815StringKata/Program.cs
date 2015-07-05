ca853354d537d81b038793345be9e77a8320c74d
Joey B Capolongo <club4238@hotmail.com>
2015-07-05 13:14:44 -0400

    Kata work 6/28 (Boiler Plate) & 7/5 joint session
    Clarify my instructions for what is expected when doing my daily Kata.

---
+++ 062815StringKata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _062815StringKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = new string[5] {"100", "123,4", "", "10000000000,6", "1030484"};
+
+            foreach (var input in inputs)
+            {
+                Console.WriteLine(String.Format("Sum of {0} is {1}.", input, Add(input).ToString()));
+            }
+        }
+        static int Add(string numbers)
+        {
+            return 0;
+        }
+    } 
+}
---
+++ 070515Kata/070515Kata/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _070515Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = new string[5] { "100", "123,4", "", "10000000000,6", "1030484" };
+
+            foreach (var input in inputs)
+            {
+                Console.WriteLine(String.Format("Sum of {0} is {1}.", input, Add(input).ToString()));
+            }
+        }
+        static int Add(string numbers)
+        {
+            var splitNumbers = numbers.Split(new char[] { ',' });
+            var convertedNumbers = new int[splitNumbers.Length]; 
+                
+            
+            return 0;
+        }
+    }
+}
